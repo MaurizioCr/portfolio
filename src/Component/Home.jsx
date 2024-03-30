836c030217fc99bbb543c4942ac7605a49b10dc8
MaurizioCr <crispinomaurizio034@gmail.com>
2024-03-30 20:26:13 +0100

    Visible

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,19 +1,35 @@
-import { Col, Container, Row } from "react-bootstrap"
+import React, { useState } from "react";
+import { Col, Container, Row, Button } from "react-bootstrap";
 
-const Blocco = () =>{
+const Blocco = () => {
+    const [isVisible, setIsVisible] = useState(true);
 
-    return(
+    const toggleVisibility = () => {
+        setIsVisible(!isVisible);
+    };
+
+    return (
         <>
             <Container className="text-center mt-5">
                 <Row>
-                    <div className="bg-secondary " style={{width: "40rem", height: "30rem"}}>
-                        <Col className="text-center pt-5 mt-5">
-                            Benvenuto nel mio Portfolio! Mi presento, mi chiamo Maurizio Crispino, ho 19 anni e mi piace programmare!
-                        </Col>
-                    </div>
+                    {isVisible && (
+                        <div className="bg-secondary " style={{ width: "40rem", height: "30rem" }}>
+                            <Col className="text-center pt-5 mt-5">
+                                Benvenuto nel mio Portfolio! Mi presento, mi chiamo Maurizio Crispino, ho 19 anni e mi piace programmare!
+                            </Col>
+                        </div>
+                    )}
+                </Row>
+                <Row className="mt-3">
+                    <Col className="text-center">
+                        <Button onClick={toggleVisibility}>
+                            {isVisible ? "Nascondi" : "Mostra"}
+                        </Button>
+                    </Col>
                 </Row>
             </Container>
         </>
-    )
-}
-export default Blocco
+    );
+};
+
+export default Blocco;
