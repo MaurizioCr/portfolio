9bcbd1a8e0cb2165d8b24bc069bcc0fdef11c774
MaurizioCr <crispinomaurizio034@gmail.com>
2024-04-07 21:33:58 +0200

    App blocco

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+      <Route element={<Blocco/>}path="/blocco"/>
         <Route element={<Home />} path="/" />
       </Routes>
     </BrowserRouter>
