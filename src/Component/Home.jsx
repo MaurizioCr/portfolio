c371ff1d9701b4530c5287f6f525983f87e171b3
MaurizioCr <crispinomaurizio034@gmail.com>
2024-05-02 17:29:04 +0200

    Collegamenti

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -164,13 +164,13 @@ const Home = () => {
                 <h1 className=" text-center">Contattami</h1>
                 <p>crispinomaurizio034@gmail.com</p>
                 <Col className="col-3 mt-5">
-                        <img src="https://cdn.icon-icons.com/icons2/3053/PNG/512/discord_macos_bigsur_icon_190238.png" alt="Windows" width={30 + "%"} height={100 + "%"}/>
+                        <a href=""><img src="https://cdn.icon-icons.com/icons2/3053/PNG/512/discord_macos_bigsur_icon_190238.png" alt="Discord" width={30 + "%"} height={100 + "%"}/></a>
                     </Col>
                 <Col className="col-3 mt-5">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png" alt="Windows" width={30 + "%"} height={100 + "%"}/>
+                        <a href="https://www.linkedin.com/in/mauriziocrispino-webdeveloper/"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png" alt="Linkedin" width={30 + "%"} height={100 + "%"}/></a>
                     </Col>
                 <Col className="col-3 mt-5">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/1200px-GitHub_Invertocat_Logo.svg.png" alt="Windows" width={30 + "%"} height={100 + "%"}/>
+                        <a href="https://github.com/MaurizioCr"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/1200px-GitHub_Invertocat_Logo.svg.png" alt="Github" width={30 + "%"} height={100 + "%"}/></a>
                     </Col>
                 
                 </AnimatedRow3>
