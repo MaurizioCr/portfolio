e14aa24c2b557d32e7fc974661f4f4c80c349684
MaurizioCr <crispinomaurizio034@gmail.com>
2024-05-14 15:53:43 +0200

    fic nomi link

--- src/Component/MyNavbar.jsx
+++ src/Component/MyNavbar.jsx
@@ -7,11 +7,11 @@ const MyNavbar = () => {
     <>
       <Navbar bg="primary" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">Me</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#home">Competenze</Nav.Link>
+            <Nav.Link href="#features">Progetti Personali</Nav.Link>
+            <Nav.Link href="#pricing">Contatti</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
