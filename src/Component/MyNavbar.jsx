cef26659c6d8bcce67495e55b54e6f5932d6d42a
MaurizioCr <crispinomaurizio034@gmail.com>
2024-05-23 17:16:37 +0200

    bg nav

--- src/Component/MyNavbar.jsx
+++ src/Component/MyNavbar.jsx
@@ -5,7 +5,7 @@ import Navbar from 'react-bootstrap/Navbar';
 const MyNavbar = () => {
   return (
     <>
-      <Navbar bg="primary" data-bs-theme="dark">
+      <Navbar bg="success" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">Me</Navbar.Brand>
           <Nav className="me-auto">
